48f59297bed49047bd5aabc626e0bb4beb64b819
LolisEqualLife <60212319+LolisEqualLife@users.noreply.github.com>
2020-01-24 11:17:51 -0800

    1 done

--- natsuki/Cogs/ask.py
+++ natsuki/Cogs/ask.py
@@ -11,9 +11,9 @@ class Ask(client.Cog):#Class thing no touchy!!!111
     @client.command()
     async def ask(self,ctx, arg1=None): # we make arg1 so we can have the command as this "n_ask my dad is in jail lmao" and it will obviously respond, if your missing the "answer arg" which comes after the command then the command will obviously not run
         if arg1 is None:
-            await ctx.send("Hey! You wanted to ask me something so what is it?!")
+            await ctx.send("You can ask the great Satanichia Kurumizawa McDowell anything!")
         else:
-            answer_list = ["Eh. Probably not.","I guess so.","How should I know, dummy?", "I don't know. Ask Monika if you want the answer that badly.", "No.","Pfft. In your dreams!", "Is manga literature?", "Yuri might know."]
+            answer_list = ["Well, probably not, but thats just me.","I guess so.","This stumps even me. I do not know.", "I do not know, try asking the second best demon, Vigne!", "I say, no.","Haha, you wish!", "Yes! It's as yes as I am demon!...", "Gabriel might know..."]
             async with ctx.message.channel.typing():
                 await asyncio.sleep(0.4) 
             await ctx.send(random.choice(answer_list))
